67cf9843aec86e74869d3fa2eb4a4800b3fe21a5
martin080 <45700603+martin080@users.noreply.github.com>
2019-07-08 17:05:00 +0300

    Update data_base.h

--- server/data_base.h
+++ server/data_base.h
@@ -4,10 +4,10 @@
 
 int load_database(const char *, json_t *);
 
-int create(json_t *, const char *, const char *, int);
+int _create(json_t *, const char *, const char *, int);
 
-int update(json_t *, const char *, int);
+int _update(json_t *, const char *, int);
 
 int _delete(json_t *, int *);
 
-char* read(json_t *, int *);
+char* _read(json_t *, int *);
